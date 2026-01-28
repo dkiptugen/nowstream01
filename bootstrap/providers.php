5728efa6812777bfba8290afe05d9cce78470f2c
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-01-28 12:25:46 +0300

    test

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,14 +4,12 @@ use App\Providers\AppServiceProvider;
 use App\Providers\BroadcastServiceProvider;
 use App\Providers\EventServiceProvider;
 use App\Providers\MacrosServiceProvider;
-use App\Providers\RouteServiceProvider;
 use \Stevebauman\Location\LocationServiceProvider;
 
 return [
     AppServiceProvider::class,
     BroadcastServiceProvider::class,
     EventServiceProvider::class,
-    RouteServiceProvider::class,
     MacrosServiceProvider::class,
     LocationServiceProvider::class,
 ];
