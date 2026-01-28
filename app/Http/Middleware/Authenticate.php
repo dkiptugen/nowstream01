823425fd5480b144a282389cbd0b294621c7a5eb
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-01-28 22:24:42 +0300

    test

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -23,7 +23,7 @@ class Authenticate extends Middleware
                 if ($request->expectsJson()) {
                     return null;
                 }
-                dd($this->guards);
+                dd($this->guard);
                 if (in_array('admin', $this->guards)) {
                     return route('admin.login');
                 }
