c3fa31294de003f2233bf3a1a6275bbd742701b8
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-01-28 22:51:14 +0300

    test

--- resources/views/Frontend/includes/header.blade.php
+++ resources/views/Frontend/includes/header.blade.php
@@ -62,7 +62,7 @@
     </script>
 
     <title>
-        {{ $title ?? 'Baze Live'}}
+        {{ $title ?? 'Nowstream'}}
     </title>
 
 
@@ -82,7 +82,7 @@
 
                         <div class="topbar-logo-header d-flex align-content-center text-left">
                             <a href="{{url('/')}}">
-                                <img src="{{ asset('logo1.png') }}" class="logo-icon" alt="Baze Live Logo">
+                                <img src="{{ asset('nowstream-light.png') }}" class="logo-icon" alt="Baze Live Logo">
                             </a>
                         </div>
                         <!-- <div class="mobile-toggle-menu d-block d-lg-none" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"><i class='bx bx-menu'></i></div> -->
