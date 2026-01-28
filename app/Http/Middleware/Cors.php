80f901725f7adb2a24d923be7c14e49b6d6bf34a
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-01-28 21:57:22 +0300

    test

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,11 +1,11 @@
 <?php
-	
+
 	namespace App\Http\Middleware;
-	
+
 	use Closure;
 	use Illuminate\Http\Request;
 	use Symfony\Component\HttpFoundation\Response;
-	
+
 	class Cors
 		{
 		/**
@@ -16,20 +16,19 @@
 			public function handle (Request $request, Closure $next)
 			: Response
 				{
-					/*  return $next($request)
-						  ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
-						  ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');*/
-					$response = $next($request);
-					
-					$response->headers->set ('Access-Control-Allow-Origin', '*');
-					$response->headers->set ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-					$response->headers->set ('Access-Control-Allow-Headers',
-					                         'x-requested-with, content-type, X-Auth-Token, Authorization, origin, accept');
-					// Handle preflight requests
-					if ($request->getMethod() == "OPTIONS") {
-						$response->setStatusCode(200);
-					}
-					
-					return $response;
-				}
+                    // Handle preflight requests
+                    if ($request->isMethod('OPTIONS')) {
+                        return response('', 204)
+                            ->header('Access-Control-Allow-Origin', '*')
+                            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                            ->header('Access-Control-Allow-Headers', '*');
+                    }
+
+                    $response = $next($request);
+
+                    // ALWAYS return a response
+                    return $response->header('Access-Control-Allow-Origin', '*')
+                        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                        ->header('Access-Control-Allow-Headers', '*');
+                }
 		}
