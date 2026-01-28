2d1060524325846c00ce966b4681e95b2bb2f963
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-01-28 14:21:50 +0300

    test

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -1,7 +1,7 @@
 <?php
-	
+
 	namespace App\Console\Commands;
-	
+
 
 	use App\Libs\AfricasTalking;
 	use App\Libs\SafaricomContent;
@@ -12,8 +12,8 @@
 	use Illuminate\Console\Command;
 	use Illuminate\Support\Facades\File;
 	use Illuminate\Support\Facades\Storage;
-	
-	
+
+
 	class Test extends Command
 		{
 			use Helper;
@@ -23,42 +23,41 @@
 		 * @var string
 		 */
 			protected $signature = 'app:test';
-		
+
 		/**
 		 * The console command description.
 		 *
 		 * @var string
 		 */
 			protected $description = 'Command description';
-		
+
 		/**
 		 * Execute the console command.
 		 */
 
 			public function handle ()
 				{
-					$file = Storage::disk('videos')->get('007.mp4');
-					$this->info($file);
+					/*$file = Storage::disk('videos')->get('007.mp4');
+					$this->info($file);*/
 					/*	$test = '+254713154085';
 					$this->info(substr ($test,-9,3));*/
-				/*
+
 					$at = new AfricasTalking();
 					$mes= $at->send_sms ('baze','0713154085','Test Message from baze live');
-					dd($mes);
-					*/
+					$this->info($mes);
 					/*$user = (new SystemUser())->updateOrCreate ([
 																	'email' => 'info@bazelive.co.ke'
 																], [
-																	
+
 																	"name" => "Baze Live API User",
 																	"password" => bcrypt ('RE9f98allANpxgBJlmzxJTs8bmE2U2C3'),
 																	"status" => true,
 																	"type" => 'owner'
-																
+
 																]);*/
 					/*$directoryPath = public_path ('videos'); // Replace 'your-directory' with the path to your directory
 					$files         = File::files ($directoryPath);
-					
+
 					foreach ($files as $file)
 						{
 							$name  = $file->getFilename ();
