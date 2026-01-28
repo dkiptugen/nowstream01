9c954e6a8ec2da729a9887b70c045d4a1df2bcbd
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-01-28 22:39:04 +0300

    test

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,26 +11,41 @@ class Authenticate extends Middleware
     {
         public function handle($request, Closure $next, ...$guards)
             {
-                dd($guards);
-                foreach ($guards as $guard) {
-                    if (Auth::guard($guard)->check()) {
-                        Auth::shouldUse($guard);
-                        return $next($request);
+
+                foreach ($guards as $guard)
+                    {
+                        if (Auth::guard($guard)->check())
+                            {
+                                Auth::shouldUse($guard);
+                                return $next($request);
+                            }
+                        else
+                            {
+                                if (Auth::guard('admin')->guest())
+                                    {
+                                        return route('admin.login');
+                                    }
+                                if (Auth::guard('user')->guest() || Auth::guard('web')->guest())
+                                    {
+                                        return route('user.login');
+                                    }
+                            }
                     }
-                }
 
                 return $this->unauthenticated($request, $guards);
 
             }
+
     /**
      * Determine the path to redirect unauthenticated users.
      */
         protected function redirectTo(Request $request): ?string
             {
                 // If this is an API request, do not redirect
-                if ($request->expectsJson()) {
-                    return null;
-                }
+                if ($request->expectsJson())
+                    {
+                        return null;
+                    }
                 if (Auth::guard('admin')->guest())
                     {
                         return route('admin.login');
