971c0c9cbce420bb668d94a21dc1aee0966cd6ff
OPrestone <84594819+OPrestone@users.noreply.github.com>
2026-02-03 13:38:18 +0300

    new design

--- database/migrations/2026_02_02_171740_create_comment_likes_table.php
+++ database/migrations/2026_02_02_171740_create_comment_likes_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->enum('type', ['like', 'dislike']);
             $table->unique(['comment_id', 'user_id']);
             $table->timestamps();
-            $table->unique(['comment_id', 'user_id']); 
         });
         
     }
