61c5050cc2e7c4554d4e56628b9be18922ddec9f
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-02-09 09:35:13 +0300

    migration cleanup

--- app/Http/Datatables/SysUserDatatable.php
+++ app/Http/Datatables/SysUserDatatable.php
@@ -90,13 +90,13 @@ class SysUserDatatable
         $button = null;
         if ($request->user()->can('edit_event'))
         {
-            $button .= '<a class="text text-dark" href="' . route('user.edit', $post->id) . '" data-toggle="tooltip" title="Edit User">
+            $button .= '<a class="text text-dark" href="' . route('backend.user.edit', $post->id) . '" data-toggle="tooltip" title="Edit User">
                 <i class="fas fa-edit"></i> Edit
                 </a>';
         }
         if ($request->user()->can('destroy_event'))
         {
-            $button .= '<form id="delete-form-' . $post->id . '" action="' . route('user.destroy', $post->id) . '" method="POST" class=" create-form my-0 py-0">
+            $button .= '<form id="delete-form-' . $post->id . '" action="' . route('backend.user.destroy', $post->id) . '" method="POST" class=" create-form my-0 py-0">
                 <input type="hidden" name="_token" value="' . csrf_token() . '" />
                 <input type="hidden" name="_method" value="DELETE" class="my-0 py-0" />
                 <button type="submit" class="btn btn-link text-dark" data-toggle="tooltip" title="Delete User"><i class="fas fa-trash"></i> Delete</button>
