391ce1f6b006f9de13f48deb7f27a9c8898c8d03
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-02-10 22:57:14 +0300

    check

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+        use HasUuid;
+        protected $keyType = 'string';
+        public $incrementing = false;
+        protected $primaryKey='uuid';
 }
