9fc3098216154dfd75efb363a3ffbc467003dee8
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-02-12 16:11:03 +0300

    podcast,stream

--- app/Http/Controllers/Backend/PodcastEpisodeController.php
+++ app/Http/Controllers/Backend/PodcastEpisodeController.php
@@ -3,9 +3,115 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Datatables\PodcastDatatable;
+use App\Http\Datatables\PodcastEpisodeDatatable;
+use App\Http\Services\UploadService;
+use App\Models\Category;
+use App\Models\Content;
+use App\Models\Region;
+use App\Models\Tag;
+use App\Traits\Meta;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class PodcastEpisodeController extends Controller
 {
-    //
+        use Meta;
+        public $data = [];
+        public function __construct()
+            {
+                $this->data = self::product_def();
+            }
+
+    /**
+     * Display a listing of the resource.
+     */
+        public function index(Content $podcast)
+            {
+                $this->data['podcast'] = $podcast;
+                return view('Backend.modules.episode.index', $this->data);
+            }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+        public function create(Content $podcast)
+            {
+                $this->data['podcast'] = $podcast;
+                return view('Backend.modules.episode.add', $this->data);
+            }
+
+        public function store(Content $podcast,Request $request)
+            {
+
+            }
+
+
+    /**
+     * Display the specified resource.
+     */
+        public function show(Content $podcast)
+            {
+                //
+            }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+        public function edit(Content $podcast, Content $episode)
+            {
+                $this->data['category'] = Category::get();
+                $this->data['region'] = Region::get();
+                $this->data['podcast']  = $podcast;
+                $this->data['episode']  = $episode->load('tags','categories');
+                return view('Backend.modules.episode.edit', $this->data);
+            }
+
+    /**
+     * Update the specified resource in storage.
+     */
+        public function update(Request $request, Content $podcast, Content $episode)
+            {
+
+            }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+        public function destroy(Content $podcast, Content $episode)
+            {
+                $result = $episode->delete();
+                if ($result)
+                    {
+                        return self::success('Videos', 'Video deleted successfully.',
+                            route('backend.podcast.episode.index',['podcast'=>$podcast->uuid])
+                        );
+                    }
+                else
+                    {
+
+                        return self::failed('Channel videos', 'Video not deleted.',
+                            route('backend.podcast.episode.index',['podcast'=>$podcast->uuid])
+                        );
+                    }
+            }
+
+
+    /**
+     * Custom method added for datatable.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+        public function datatable(Request $request,Content $podcast, PodcastEpisodeDatatable $datatable)
+            {
+                $datatable->columns = [
+                    0 => 'id',
+                    1 => 'title',
+                    2 => 'description',
+                    6 => 'created_at'
+                ];
+                return response()->json($datatable->data($request, $podcast->uuid));
+            }
 }
---
+++ app/Http/Datatables/PodcastEpisodeDatatable.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Datatables;
+
+use App\Models\Event;
+use App\Traits\Helper;
+use App\Models\Content;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+
+class PodcastEpisodeDatatable
+    {
+        use Helper;
+
+        public $columns = [];
+
+    /**
+     * Datatable JSON
+     */
+        public function data($request,$podcast_id): array
+            {
+                $limit = (int)$request->input('length', 10);
+                $start = (int)$request->input('start', 0);
+                $draw  = (int)$request->input('draw');
+
+                $orderIndex = (int)$request->input('order.0.column', 0);
+                $dir        = $request->input('order.0.dir', 'desc') === 'asc' ? 'asc' : 'desc';
+
+                $orderColumn = $this->columns[$orderIndex] ?? 'id';
+
+                $baseQuery = Content::query()
+                                    ->with(['categories:uuid,name', 'tags:id,name'])// Eager load
+                                    ->where('content_group', 'podcast_episode')
+                                    ->where('parent_id',$podcast_id);
+
+                $totalData = (clone $baseQuery)->count();
+
+                /**
+                 * SEARCH
+                 */
+                if ($search = $request->input('search.value'))
+                    {
+                        $baseQuery->where(function (Builder $query) use ($search)
+                            {
+                                $query
+                                    ->where('title', 'LIKE', "%{$search}%")
+                                    ->orWhere('description', 'LIKE', "%{$search}%");
+                            });
+                    }
+
+                $totalFiltered = (clone $baseQuery)->count();
+
+                /**
+                 * FETCH DATA
+                 */
+                $posts = $baseQuery
+                    ->orderBy($orderColumn, $dir)
+                    ->offset($start)
+                    ->limit($limit)
+                    ->get();
+
+                $data = [];
+                $pos  = $start + 1;
+
+                foreach ($posts as $post)
+                    {
+                        $data[] = [
+                            'pos'          => $pos++,
+                            'title'       => e($post->title),
+                            'description' => str($post->description)->limit(50),
+                            'thumbnail'   => '<img src="'.$post->thumbnail_url.'" class="img-fluid" />',
+                            'category'    => $post->categories?->pluck('name')->implode(', '),
+                            'keywords'    => $post->tags?->pluck('name')->implode(', '),
+                            'source'      => e($post->source),
+                            'episodes'    => '<a href="' . route('backend.podcast.episode.index',['podcast'=>$post->uuid]) . '" class="text-dark text-underline text-bold">'
+                                . $post->children_count .
+                                '</a>',
+                            'publishdate' => $post->publishdate,
+                            'status'      => $post->status ? 'active' : 'inactive',
+                            'action'      => $this->button($post, $request),
+                        ];
+                    }
+
+                return [
+                    'draw'            => $draw,
+                    'recordsTotal'    => $totalData,
+                    'recordsFiltered' => $totalFiltered,
+                    'data'            => $data,
+                ];
+            }
+
+
+    /**
+     * @param $post
+     * @param $request
+     *
+     * @return string
+     */
+        private function button($post, $request)
+            {
+                $button = null;
+                if ($request->user()->can('edit_podcast_episode'))
+                    {
+                        $button .= '<a class="text text-dark" href="' . route('backend.podcast.episode.edit', ['podcast' => $post->parent_id,'episode'=>$post->uuid]) . '" data-toggle="tooltip" title="Edit podcast Episode">
+                <i class="fas fa-edit"></i> Edit
+                </a>';
+                    }
+                if ($request->user()->can('destroy_podcast_episode'))
+                    {
+                        $button .= '<form id="delete-form-' . $post->id . '" action="' . route('backend.podcast.episode.destroy', ['podcast' => $post->parent_id,'episode'=>$post->uuid]) . '" method="POST" class=" create-form my-0 py-0">
+                <input type="hidden" name="_token" value="' . csrf_token() . '" />
+                <input type="hidden" name="_method" value="DELETE" class="my-0 py-0" />
+                <button type="submit" class="btn btn-link text-dark" data-toggle="tooltip" title="Delete Podcast Episode"><i class="fas fa-trash"></i> Delete</button>
+                </form>';
+                    }
+
+                return '<div class="d-flex align-items-center">' . $button . "</div>";
+            }
+    }
