cfb7accb6cd121235abfe52661a7949e7c2cb163
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-02-12 16:30:11 +0300

    podcast,stream

--- resources/views/Backend/includes/sidebar.blade.php
+++ resources/views/Backend/includes/sidebar.blade.php
@@ -52,7 +52,7 @@
                 @can('view_tv')
                     <li class="sidebar-item">
                         <a href="{{ route('backend.tv.index') }}" class="  sidebar-link">
-                           <i class="fas fa-screen align-middle"></i>
+                           <i class="fas fa-tv align-middle"></i>
                             <span class="align-middle">TVs</span>
                         </a>
 
@@ -70,7 +70,7 @@
                 @can('view_podcast')
                     <li class="sidebar-item">
                         <a href="{{ route('backend.podcast.index') }}" class="  sidebar-link">
-                           <i class="fas fa-screen align-middle"></i>
+                           <i class="fas fa-volume align-middle"></i>
                             <span class="align-middle">Podcasts</span>
                         </a>
 
