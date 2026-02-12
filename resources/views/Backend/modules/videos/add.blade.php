6d6d190c2073f26496039efae40b4239ba782701
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-02-12 15:53:02 +0300

    podcast,stream

--- resources/views/Backend/modules/videos/add.blade.php
+++ resources/views/Backend/modules/videos/add.blade.php
@@ -28,7 +28,7 @@
 
                         <div class="form-group">
         <label for="title" class="control-label">Video Title</label>
-        <input type="text" name="title" id="title" class="form-control@error('title') is-invalid @enderror" value="{{ old('title') }}">
+        <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}">
                             @error('title')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
