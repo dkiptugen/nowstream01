857bf5b6050d6f41ae950f959b6e5d78e40db683
OPrestone <84594819+OPrestone@users.noreply.github.com>
2026-02-13 14:04:47 +0300

    podcasts

--- resources/views/Frontend/includes/nav.blade.php
+++ resources/views/Frontend/includes/nav.blade.php
@@ -46,6 +46,11 @@
                 <div class="menu-title">Podcasts</div>
             </a> 
         </li>
+        <li class="nav-item"> <a class="nav-link" href=" {{ url('tvs') }}">
+                <div class="parent-icon"><i class='bx bx-microphone'></i> </div>
+                <div class="menu-title">Live TV</div>
+            </a> 
+        </li>
         <li class="nav-item">
             <a class="nav-link" href=" {{ url('/favorites') }}">
                 <div class="parent-icon"><i class="bx bx-heart"></i>
