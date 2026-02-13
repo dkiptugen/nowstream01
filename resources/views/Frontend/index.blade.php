1a3ca00b1ffe57fd82280136e58dc27f33eef523
oprestone <oprestone0@gmail.com>
2026-02-13 19:47:40 +0300

    d

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -273,39 +273,7 @@
         <div class="row tr-movie-active">
 
             @foreach($topPodcasts as $podcast)
-            <div class="col-xl-2 col-lg-2 col-sm-6 grid-item grid-sizer">
-                <div class="movie-item mb-60">
-                    <div class="movie-poster">
-                        <a href="{{ route('podcast.show', [$podcast->uuid, $podcast->slug]) }}">
-                            <img src="{{$podcast->thumbnail_url ?? asset('frontend-assets/images/default.png')}}"
-                                class="w-100 d-block w-100" alt="...">
-                            <div class="play fs-40">
-                                <i class="fadeIn animated bx bx-play-circle"></i>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="movie-content">
-                        <div class="top">
-                            <h6 class=" mt-0">
-                                <a href="{{ url("/podcast/{$podcast->id}/{$podcast->slug}") }}">
-                                    {{ucfirst($podcast->title)}}
-                                </a>
-                            </h6>
-                        </div>
-                        <div class="bottom">
-                            <!-- Display number of views -->
-
-                            <ul>
-                                <li>
-                                    <span class="channel"><i class="far fa-user"></i>
-                                        {{ $podcast->author ? $podcast->author : 'Unknown' }}</span>
-                                    <span class="rating"><i class="fas fa-thumbs-up"></i> 3.5</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('Frontend.includes.components.cards.podcast-card')
             @endforeach
         </div>
         <div class="row align-items-end mb-30">
@@ -319,39 +287,7 @@
         <div class="row tr-movie-active">
 
             @foreach($podcasts as $podcast)
-            <div class="col-xl-2 col-lg-2 col-sm-6 grid-item grid-sizer">
-                <div class="movie-item mb-60 h-100">
-                    <div class="movie-poster">
-                        <a href="{{ route('podcast.show', [$podcast->uuid, $podcast->slug]) }}">
-                            <img src="{{$podcast->thumbnail_url ?? asset('frontend-assets/images/default.png')}}"
-                                class="w-100 d-block w-100" alt="...">
-                            <div class="play fs-40">
-                                <i class="fadeIn animated bx bx-play-circle"></i>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="movie-content">
-                        <div class="top">
-                            <h6 class="clamp-2 mt-0">
-                                <a href="{{ url("/podcast/{$podcast->id}/{$podcast->slug}") }}">
-                                    {{ucfirst($podcast->title)}}
-                                </a>
-                            </h6>
-                        </div>
-                        <div class="bottom">
-                            <!-- Display number of views -->
-
-                            <ul>
-                                <li>
-                                    <span class="channel"><i class="far fa-user"></i>
-                                        {{ $podcast->author ? $podcast->author : 'Unknown' }}</span>
-                                    <span class="rating"><i class="fas fa-thumbs-up"></i> 3.5</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('Frontend.includes.components.cards.podcast-card')
             @endforeach
         </div>
     </div>
