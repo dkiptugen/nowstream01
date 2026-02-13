d8c3105f5cfa28274124abaf70a10cba18b90bd8
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-02-13 18:15:17 +0300

    episode

--- app/Traits/Meta.php
+++ app/Traits/Meta.php
@@ -26,8 +26,8 @@
                         'name' => 'Streamer',
                         'title' => 'Streamer: Universe of Entertainment',
                         'description' => 'Explore the limitless possibilities. ',
-                        'logo' => asset('assets/img/logo/logo.png'),
-                        'image' => asset('assets/img/logo/logo.png'),
+                        'logo' => asset('assets/img/logo/logo-d.png'),
+                        'image' => asset('assets/img/logo/logo-d.png'),
                         'keywords' => 'Now Content,',
                         'author' => 'Caydeesoft Solutions Limited'
                     ];
