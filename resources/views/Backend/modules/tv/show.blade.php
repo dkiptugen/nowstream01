6c5bb6d0815d1056eadaafaf2e25e9d1bc9fe1fd
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-02-13 14:12:39 +0300

    iptv

--- resources/views/Backend/modules/tv/show.blade.php
+++ resources/views/Backend/modules/tv/show.blade.php
@@ -38,7 +38,7 @@
                     </div>
                     <div class="card-body">
                         <div class="embed-responsive embed-responsive-16by9">
-                            <video id="player" class="embed-responsive-item" playsinline data-poster="{{ $tv->thumbnail_url }}" controls crossorigin></video>
+                            <video id="player" class="embed-responsive-item" playsinline crossorigin></video>
                         </div>
                         <h3>{{$tv->title}}</h3>
 
@@ -50,77 +50,37 @@
 
     @endsection
     @section('header')
-
+        <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
     @endsection
     @section('footer')
         <!-- HLS.js (for .m3u8 support) -->
         <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 
+        <!-- Plyr JS -->
+        <script src="https://cdn.plyr.io/3.7.8/plyr.polyfilled.js"></script>
         <script>
             document.addEventListener('DOMContentLoaded', () => {
                 const video = document.getElementById('player');
                 const source = '{{ $tv->stream_url }}';
+
                 let player;
 
-                function initPlayer() {
-                    player = new Plyr(video);
-                }
+                if (source.endsWith('.m3u8') && Hls.isSupported())
+                    {
 
-                function loadHLS(url) {
-                    if (Hls.isSupported()) {
-                        const hls = new Hls({
-                            maxBufferLength: 30,
-                            maxMaxBufferLength: 60,
-                            enableWorker: true
-                        });
-
-                        hls.loadSource(url);
+                        const hls = new Hls();
+                        hls.loadSource(source);
                         hls.attachMedia(video);
 
                         hls.on(Hls.Events.MANIFEST_PARSED, function () {
-                            initPlayer();
+                            player = new Plyr(video);
                         });
-
-                        hls.on(Hls.Events.ERROR, function (event, data) {
-                            if (data.fatal) {
-                                console.error('Fatal HLS error:', data);
-                                switch (data.type) {
-                                    case Hls.ErrorTypes.NETWORK_ERROR:
-                                        hls.startLoad();
-                                        break;
-                                    case Hls.ErrorTypes.MEDIA_ERROR:
-                                        hls.recoverMediaError();
-                                        break;
-                                    default:
-                                        hls.destroy();
-                                        break;
-                                }
-                            }
-                        });
-
                     }
-                    else if (video.canPlayType('application/vnd.apple.mpegurl'))
-                    {
-                        video.src = url;
-                        initPlayer();
-                    }
-                    else {
-                        console.error('HLS not supported in this browser');
-                    }
-                }
-
-                function loadMP4(url) {
-                    video.src = url;
-                    initPlayer();
-                }
-
-                if (source.endsWith('.m3u8')) {
-                    loadHLS(source);
-                } else {
-                    loadMP4(source);
+                else {
+                    video.src = source;
+                    player = new Plyr(video);
                 }
             });
         </script>
-
 
 @endsection
