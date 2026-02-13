2fff62707bd2de1b9d9a47c54d9cfb2a8f914cde
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-02-13 20:02:28 +0300

    radio

--- app/Http/Datatables/RadioDatatable.php
+++ app/Http/Datatables/RadioDatatable.php
@@ -50,7 +50,7 @@
                                     $nestedData['pos']         = $pos;
                                     $nestedData['title']       = $post->title;
                                     $nestedData['description'] = $post->description;
-                                    $nestedData["thumbnail"]   = $this->thumbnail_tag($post->thumbnail_url, 'img-fluid', 'height:50px; width:50px');
+                                    $nestedData["thumbnail"]   = '<img src="'.$post->thumbnail_url.'" class=""img-fluid" style="height:50px; width:50px" />';
                                     $nestedData['language']    = $post->language;
                                     $nestedData['region']      = $post->country;
                                     $nestedData['category']    = $post->categories?->pluck('name')->implode(', ');
