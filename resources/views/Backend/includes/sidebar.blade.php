d0084dac163bb59966b999d7ad3b19574f14c9c7
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-02-13 18:19:01 +0300

    check

--- resources/views/Backend/includes/sidebar.blade.php
+++ resources/views/Backend/includes/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="wrapper">
     <nav class="sidebar sidebar-sticky">
         <span class="sidebar-content  js-simplebar">
-            <a class="sidebar-brand text-center" href="{{ route('backend.admin_dashboard') }}">
-                <img src="{{ asset($logo) }}" alt="Logo" class="img-fluid w-100"  >
+            <a class="sidebar-brand" href="{{ route('backend.admin_dashboard') }}" >
+                <img src="{{ asset($logo) }}?=1" alt="Logo" class="img-fluid w-100"  >
             </a>
 
             <ul class="sidebar-nav">
