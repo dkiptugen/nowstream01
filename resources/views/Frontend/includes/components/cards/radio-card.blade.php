a5bddb868a4424fe1594ac7087b111aca57046ed
oprestone <oprestone0@gmail.com>
2026-02-14 10:51:45 +0300

    sdac

--- resources/views/Frontend/includes/components/cards/radio-card.blade.php
+++ resources/views/Frontend/includes/components/cards/radio-card.blade.php
@@ -1,27 +1,24 @@
-<div class="col-xl-2 col-lg-2 col-sm-6 grid-item grid-sizer">
-                <div class="movie-item mb-60">
-                    <div class="movie-poster">
-                        <a href="{{ route('radio.show', [$radio->uuid, $radio->slug]) }}"  
-    onclick="playSingleAudio(
+<div class="col-xl-2 col-lg-2 col-sm-6 grid-item grid-sizer"
+     onclick="playSingleAudio(
         '{{ $radio->stream_url }}',
         '{{ addslashes($radio->title) }}',
         'Live Radio',
         '{{ $radio->thumbnail_url ?? asset('assets/img/default-thumbnail.jpg') }}'
-    )"
-    >
+     )"
+     style="cursor:pointer;">
+                <div class="movie-item mb-60">
+                    <div class="movie-poster">
+                        
                             <img src="{{$radio->thumbnail_url ?? asset('frontend-assets/images/default.png')}}"
                                 class="w-100 d-block w-100" alt="..." style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
                             <div class="play fs-40">
                                 <i class="fadeIn animated bx bx-play-circle"></i>
-                            </div>
-                        </a>
+                            </div> 
                     </div>
                     <div class="movie-content">
                         <div class="top">
-                            <h6 class=" mt-0">
-                        <a href="{{ route('radio.show', [$radio->uuid, $radio->slug]) }}">
-                                    {{ucfirst($radio->title)}}
-                                </a>
+                            <h6 class=" mt-0"> 
+                                    {{ucfirst($radio->title)}} 
                             </h6>
                         </div>
                         <div class="bottom"> 
