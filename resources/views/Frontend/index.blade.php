0d22382ee3e7c73447296190425915a2427b2835
OPrestone <84594819+OPrestone@users.noreply.github.com>
2026-02-16 12:42:48 +0300

    sf

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -102,12 +102,23 @@
                     </div>
                 </div>
             </div>
-            <div class="row tr-movie-active">
+            </div>
+            
+        <div class="pcar-wrapper">
 
-                @foreach($toptvs as $tv)
-                    @include('Frontend.includes.components.cards.tv-card')
-                @endforeach
+            <!-- Outside container overlays -->
+            <div class="pcar-overlay pcar-overlay-left"></div>
+            <div class="pcar-overlay pcar-overlay-right"></div>
 
+            <div class="pcar" data-autoplay="true" data-interval="3500" data-desktop="11" data-tablet="3" data-mobile="1">
+
+                <div class="pcar-track">
+                    @foreach($toptvs as $item)
+                        <div class="pcar-item">
+                            @include('Frontend.includes.components.cards.slider-card')
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </section>
