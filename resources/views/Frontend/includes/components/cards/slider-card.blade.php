fece2dc13666062f58a1fb130e9c8a2610f0b253
OPrestone <84594819+OPrestone@users.noreply.github.com>
2026-02-16 18:08:43 +0300

    sf

--- resources/views/Frontend/includes/components/cards/slider-card.blade.php
+++ resources/views/Frontend/includes/components/cards/slider-card.blade.php
@@ -1,94 +1,98 @@
-<div class="movie-wrapper">
-    
-   @php
-    $thumbnail = $item->thumbnail_url ?? asset('assets/img/default.png');
-    $title = ucfirst($item->title);
+@if ($item->content_group === 'radio')
 
-    // Map content types to routes
-    $routeMap = [
-        'video' => 'video.show',
-        'tv' => 'tv.show',
-        'radio' => 'radio.show',
-        'podcast' => 'podcast.show',
-    ];
+    <div class="movie-item" onclick="playSingleAudio(
+            '{{ $item->stream_url }}',
+            '{{ addslashes($item->title) }}',
+            'Live Radio',
+            '{{ $item->thumbnail_url ?? asset('assets/img/default-thumbnail.jpg') }}'
+         )" style="cursor:pointer;">
 
-    $routeName = $routeMap[$item->content_group] ?? null;
-    $link = $routeName ? route($routeName, [$item->uuid, $item->slug]) : '#';
-@endphp
+        <div class="movie-poster">
+            <img src="{{$item->thumbnail_url ?? asset('assets/images/default.png')}}" class="w-100 d-block w-100"
+                alt="{{ $item->title  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
+            <div class="play fs-40">
+                <i class="fadeIn animated bx bx-play-circle"></i>
+            </div>
+        </div>
+        <div class="movie-content">
+            <div class="top">
+                <h6 class=" mt-0">
+                    {{ucfirst($item->title)}}
+                </h6>
+            </div>
+        </div>
+    </div>
+@elseif ($item->content_group === 'tv')
+    <div class="movie-item">
 
-    @if($item->content_group === 'radio')
-         <div class="movie-item" style="cursor:pointer;"
-            onclick="playSingleAudio('{{ $item->stream_url }}', '{{ addslashes($title) }}', 'Live Radio', '{{ $thumbnail }}')">
-            <div class="movie-poster">
-                <img src="{{ $thumbnail }}" class="w-100 d-block" alt="{{ $title }}" style="object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
+        <div class="movie-poster">
+            <a href="{{ route($item->content_group . '.show', [$item->uuid, $item->slug]) }}">
+                <img src="{{$item->thumbnail_url ?? asset('assets/images/default.png')}}"
+                    class="w-100 d-block w-100" alt="{{ $item->title  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
                 <div class="play fs-40">
                     <i class="fadeIn animated bx bx-play-circle"></i>
                 </div>
-            </div>
-            <div class="movie-content">
-                <div class="top">
-                    <h6 class="mt-0">{{ $title }} Radio</h6>
-                </div>
-            </div>
-        </div> 
-
-    @elseif($item->content_group === 'tv' || $item->content_group === 'video')
-        <div class="movie-item">
-            <div class="movie-poster">
-                <a href="{{ $link }}">
-                    <img src="{{ $thumbnail }}" class="w-100 d-block" alt="{{ $title }}" style="object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
-                    <div class="play fs-40">
-                        <i class="fadeIn animated bx bx-play-circle"></i>
-                    </div>
-                </a>
-            </div>
-            <div class="movie-content">
-                <div class="top">
-                    <h6 class="mt-0">
-                        <a href="{{ $link }}">{{ $title }}</a>
-                    </h6>
-                </div>
+            </a>
+        </div>
+        <div class="movie-content">
+            <div class="top">
+                <h6 class=" mt-0">
+                    <a href="{{ route($item->content_group . '.show', [$item->uuid, $item->slug]) }}">
+                        {{ucfirst($item->title)}}
+                    </a>
+                </h6>
             </div>
         </div>
+    </div>
+@elseif ($item->content_group === 'podcast') 
+    <div class="movie-card">
 
-    @elseif($item->content_group === 'podcast')
-         <div class="movie-item">
-            <div class="movie-poster">
-                <a href="{{ $link }}">
-                    <img src="{{ $thumbnail }}" class="w-100 d-block" alt="{{ $title }}" style="object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
-                    <div class="play fs-40">
-                        <i class="fadeIn animated bx bx-play-circle"></i>
-                    </div>
-                </a>
-            </div>
-            <div class="movie-content">
-                <div class="top">
-                    <h6 class="mt-0">
-                        <a href="{{ $link }}">{{ $title }}</a>
-                    </h6>
+        <!-- Poster -->
+        <div class="movie-poster">
+            <a href="{{ route($item->content_group . '.show', [$item->uuid, $item->slug]) }}">
+                <img src="{{ $item->thumbnail_url ?? asset('assets/images/default.png') }}"
+                     class="movie-img" alt="{{ $item->title  }}" loading="lazy">
+                <div class="play-icon">
+                    <i class="bx bx-play-circle"></i>
                 </div>
-            </div>
-        <div class="movie-card">
-            <div class="movie-poster">
-                <a href="{{ $link }}">
-                    <img src="{{ $thumbnail }}" class="movie-img" alt="{{ $title }}" loading="lazy">
-                    <div class="play-icon">
-                        <i class="bx bx-play-circle"></i>
-                    </div>
-                </a>
-            </div>
+            </a>
         </div>
 
-    @else
-        <div class="movie-item">
-            <div class="movie-poster">
-                <img src="{{ $thumbnail }}" class="w-100 d-block" alt="{{ $title }}" style="object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
-            </div>
-            <div class="movie-content">
-                <div class="top">
-                    <h6 class="mt-0">{{ $title }} gyh</h6>
+    </div> 
+@elseif ($item->content_group === 'video')
+    <div class="movie-item">
+
+        <div class="movie-poster">
+            <a href="{{ route($item->content_group . '.show', [$item->uuid, $item->slug]) }}">
+                <img src="{{$item->thumbnail_url ?? asset('assets/images/default.png')}}"
+                    class="w-100 d-block w-100" alt="{{ $item->title  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
+                <div class="play fs-40">
+                    <i class="fadeIn animated bx bx-play-circle"></i>
                 </div>
+            </a>
+        </div>
+        <div class="movie-content">
+            <div class="top">
+                <h6 class=" mt-0">
+                    <a href="{{ route($item->content_group . '.show', [$item->uuid, $item->slug]) }}">
+                        {{ucfirst($item->title)}}
+                    </a>
+                </h6>
             </div>
         </div>
-    @endif
-</div>
+    </div>
+@else
+    <div class="movie-item">
+        <div class="movie-poster">
+            <img src="{{$item->thumbnail_url ?? asset('assets/images/default.png')}}" class="w-100 d-block w-100"
+                alt="{{ $item->title  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
+        </div>
+        <div class="movie-content">
+            <div class="top">
+                <h6 class=" mt-0">
+                    {{ucfirst($item->title)}}
+                </h6>
+            </div>
+        </div>
+    </div>
+@endif
