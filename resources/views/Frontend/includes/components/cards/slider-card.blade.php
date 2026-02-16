d76cf887a5ba9dce9e55f4c56c413e392323a3ed
OPrestone <84594819+OPrestone@users.noreply.github.com>
2026-02-16 13:06:37 +0300

    sf

--- resources/views/Frontend/includes/components/cards/slider-card.blade.php
+++ resources/views/Frontend/includes/components/cards/slider-card.blade.php
@@ -9,7 +9,7 @@
 
         <div class="movie-poster">
             <img src="{{$item->thumbnail_url ?? asset('frontend-assets/images/default.png')}}" class="w-100 d-block w-100"
-                alt="{{ Jojo Fashion House  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
+                alt="{{ $item->title  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
             <div class="play fs-40">
                 <i class="fadeIn animated bx bx-play-circle"></i>
             </div>
@@ -28,7 +28,7 @@
         <div class="movie-poster">
             <a href="{{ route($item->content_group . '.show', [$item->uuid, $item->slug]) }}">
                 <img src="{{$item->thumbnail_url ?? asset('frontend-assets/images/default.png')}}"
-                    class="w-100 d-block w-100" alt="{{ Jojo Fashion House  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
+                    class="w-100 d-block w-100" alt="{{ $item->title  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
                 <div class="play fs-40">
                     <i class="fadeIn animated bx bx-play-circle"></i>
                 </div>
@@ -51,7 +51,7 @@
         <div class="movie-poster">
             <a href="{{ route($item->content_group . '.show', [$item->uuid, $item->slug]) }}">
                 <img src="{{ $item->thumbnail_url ?? asset('frontend-assets/images/default.png') }}"
-                     class="movie-img" alt="{{ Jojo Fashion House  }}" loading="lazy">
+                     class="movie-img" alt="{{ $item->title  }}" loading="lazy">
                 <div class="play-icon">
                     <i class="bx bx-play-circle"></i>
                 </div>
@@ -65,7 +65,7 @@
         <div class="movie-poster">
             <a href="{{ route($item->content_group . '.show', [$item->uuid, $item->slug]) }}">
                 <img src="{{$item->thumbnail_url ?? asset('frontend-assets/images/default.png')}}"
-                    class="w-100 d-block w-100" alt="{{ Jojo Fashion House  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
+                    class="w-100 d-block w-100" alt="{{ $item->title  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
                 <div class="play fs-40">
                     <i class="fadeIn animated bx bx-play-circle"></i>
                 </div>
@@ -85,7 +85,7 @@
     <div class="movie-item">
         <div class="movie-poster">
             <img src="{{$item->thumbnail_url ?? asset('frontend-assets/images/default.png')}}" class="w-100 d-block w-100"
-                alt="{{ Jojo Fashion House  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
+                alt="{{ $item->title  }}" style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
         </div>
         <div class="movie-content">
             <div class="top">
