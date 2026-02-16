9f12bf37b953114b2f4a07c5c2d82c0e1d6be107
OPrestone <84594819+OPrestone@users.noreply.github.com>
2026-02-16 22:26:57 +0300

    sf

--- resources/views/Frontend/modules/tvs/index.blade.php
+++ resources/views/Frontend/modules/tvs/index.blade.php
@@ -21,7 +21,7 @@
                     <ul class="nav nav-tabs" id="myTab" role="tablist">
                             @foreach($genres as $genre)
         <li class="nav-item" role="presentation">
-                                <a class="nav-link" href="{{ route('category.show', ['slug' => Str::slug($genre)]) }}">
+                                <a class="nav-link" href="{{ route('genre.show', ['slug' => Str::slug($genre)]) }}">
             {{ ucfirst($genre) }}
         </a></li> 
     @endforeach
