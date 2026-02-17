20ae639c94e5d2f29edace81ccffdb8328cbbb63
OPrestone <84594819+OPrestone@users.noreply.github.com>
2026-02-17 13:34:33 +0300

    gd

--- resources/views/Frontend/modules/radios/show.blade.php
+++ resources/views/Frontend/modules/radios/show.blade.php
@@ -51,12 +51,11 @@
 									<span>Pg 18</span>
 									<span>hd</span>
 								</li>
-							<li class="category"> 
-    @foreach($tv->categories as $category)
-        <a href="{{ route('genre.show', $category->slug) }}">
-            {{ $category->name }}@if(!$loop->last),@endif
-        </a>
-    @endforeach
+								<li class="category"> 
+    @foreach($radio->genre as $genre) 
+        <a class="nav-link" href="{{ route('genre.show', ['genre' => Str::slug($genre)]) }}">
+            {{ ucfirst($genre) }}
+        </a> 
 </li>
 
 								<li class="release-time">
