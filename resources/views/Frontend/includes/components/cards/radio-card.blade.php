032b7d6f1522509a855e2b67081253006decad09
oprestone <oprestone0@gmail.com>
2026-02-19 01:32:51 +0300

    changes

--- resources/views/Frontend/includes/components/cards/radio-card.blade.php
+++ resources/views/Frontend/includes/components/cards/radio-card.blade.php
@@ -1,15 +1,15 @@
 <div class="w-100"
      onclick="playSingleAudio(
-        '{{ $radio->stream_url }}',
-        '{{ addslashes($radio->title) }}',
-        'Live Radio',
-        '{{ $radio->thumbnail_url ?? asset('assets/img/default-thumbnail.jpg') }}'
+        '{{ $item->stream_url }}',
+        '{{ addslashes($item->title) }}',
+        'Live item',
+        '{{ $item->thumbnail_url ?? asset('assets/img/default-thumbnail.jpg') }}'
      )"
      style="cursor:pointer;"> 
                 <div class="movie-item mb-60">
                     <div class="movie-poster">
                         
-                            <img src="{{$radio->thumbnail_url ?? asset('frontend-assets/images/default.png')}}"
+                            <img src="{{$item->thumbnail_url ?? asset('frontend-assets/images/default.png')}}"
                                 class="w-100 d-block w-100" alt="..." style=" object-fit: cover; aspect-ratio: 1/1;" loading="lazy">
                             <div class="play fs-40">
                                 <i class="fadeIn animated bx bx-play-circle"></i>
@@ -18,7 +18,7 @@
                     <div class="movie-content">
                         <div class="top">
                             <h6 class=" mt-0"> 
-                                    {{ucfirst($radio->title)}} 
+                                    {{ucfirst($item->title)}} 
                             </h6>
                         </div>
                         <div class="bottom"> 
@@ -27,7 +27,7 @@
                             <ul>
                                 <li>
                                     <span class="channel"><i class="far fa-user"></i>
-                                        {{ $radio->author ? $radio->author : 'Unknown' }}</span>
+                                        {{ $item->author ? $item->author : 'Unknown' }}</span>
                                     <span class="rating"><i class="fas fa-thumbs-up"></i> 3.5</span>
                                 </li>
                             </ul>
