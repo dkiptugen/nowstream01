5b725a8cf634d3e1e450b012019ce76311fe4c3b
oprestone <oprestone0@gmail.com>
2026-02-19 01:49:26 +0300

    changes

--- resources/views/Frontend/includes/components/partials/radio-items.blade.php
+++ resources/views/Frontend/includes/components/partials/radio-items.blade.php
@@ -1,5 +1,6 @@
 @foreach($radios as $item)
 <div class="col-xl-2 col-lg-3 col-sm-6 grid-item">
     @include('Frontend.includes.components.cards.slider-card')
+    @include('Frontend.includes.components.cards.slider-card')
 </div>
 @endforeach
