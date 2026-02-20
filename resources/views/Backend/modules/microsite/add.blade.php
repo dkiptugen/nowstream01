4bef9a9ee1f4bbcab036a08cac3c476c9431deea
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-02-20 23:03:24 +0300

    erere

--- resources/views/Backend/modules/microsite/add.blade.php
+++ resources/views/Backend/modules/microsite/add.blade.php
@@ -6,8 +6,9 @@
 
         </div>
         <div class="card-body">
-            <form action="{{ route('backend.microsite.store') }}" method="post" class="form form-horizontal create-form" enctype="multipart/form-data">
-               @csrf
+            <form action="{{ route('backend.microsite.store') }}" method="post" class="form form-horizontal create-form"
+                  enctype="multipart/form-data">
+                @csrf
                 <div class="form-group">
                     <label for="name" class="control-label">Name</label>
                     <input type="text" name="name" id="name" class="form-control">
@@ -22,45 +23,50 @@
                         <div class="input-group">
                             <div class="input-group-prepend">
                                 <span class="input-group-text">
-                                Primary Color
-                                    </span>
+                                Primary Color</span>
                             </div>
-                            <input type="color" name="colorscheme[primary]" id="colorscheme-primary" class="form-control">
+                            <input type="color" name="colorscheme[primary]" id="colorscheme-primary"
+                                   class="form-control">
                         </div>
 
 
                     </div>
                     <div class="col">
-                        <label for="colorscheme-accent" class="control-label">Accent Color</label>
-                        <input type="color" name="colorscheme[accent]" id="colorscheme-accent" class="form-control">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    Accent Color
+                                </span>
+                            </div>
+                            <input type="color" name="colorscheme[accent]" id="colorscheme-accent" class="form-control">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group form-group-file">
-                    <label for="logo" class="control-label-file">Logo</label>
-                    <input type="file" name="logo" id="logo" class="form-control-file" accept="image/*">
-                </div>
-                <div class="form-group-file">
-                    <label for="cover" class="control-label-file">Cover Image</label>
-                    <input type="file" name="cover" id="cover" class="form-control-file"  accept="image/*">
-                </div>
-                <div class="form-group-file">
-                    <label for="banner" class="control-label-file">Banner Image</label>
-                    <input type="file" name="banner" id="banner" class="form-control-file" accept="image/*">
-                </div>
-                <div class="form-group">
-                    <label for="socialmedia" class="control-label">Social Media Links</label>
-                    <input type="text" name="socialmedia" id="socialmedia_links" class="form-control tags-input">
-                </div>
-                <div class="form-group form-row">
-                    <button class="btn btn-primary btn-sm ml-auto" name="submit" value="publish"
-                            type="submit">SAVE</button>
-                </div>
+                    <div class="form-group form-group-file">
+                        <label for="logo" class="control-label-file">Logo</label>
+                        <input type="file" name="logo" id="logo" class="form-control-file" accept="image/*">
+                    </div>
+                    <div class="form-group-file">
+                        <label for="cover" class="control-label-file">Cover Image</label>
+                        <input type="file" name="cover" id="cover" class="form-control-file" accept="image/*">
+                    </div>
+                    <div class="form-group-file">
+                        <label for="banner" class="control-label-file">Banner Image</label>
+                        <input type="file" name="banner" id="banner" class="form-control-file" accept="image/*">
+                    </div>
+                    <div class="form-group">
+                        <label for="socialmedia" class="control-label">Social Media Links</label>
+                        <input type="text" name="socialmedia" id="socialmedia_links" class="form-control tags-input">
+                    </div>
+                    <div class="form-group form-row">
+                        <button class="btn btn-primary btn-sm ml-auto" name="submit" value="publish"
+                                type="submit">SAVE
+                        </button>
+                    </div>
 
 
             </form>
         </div>
     </div>
-
 
 @endsection
 @section('header')
