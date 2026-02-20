a10e0bd9962f46a0d74525d0fce706037cefc3b6
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-02-20 14:32:56 +0300

    erere

--- app/Http/Middleware/IdentifyTenant.php
+++ app/Http/Middleware/IdentifyTenant.php
@@ -9,25 +9,21 @@
         {
             public function handle($request, Closure $next)
                 {
-                    $host = $request->getHost();
+                    $host       = $request->getHost();
                     $baseDomain = config('app.base_domain');
                     // Ignore main domain
-                    if (in_array($host, [$baseDomain, 'www.'.$baseDomain]))
-                    {
-                        return $next($request);
-                    }
-
-                    // Extract subdomain
-                    $subdomain = str_replace('.'.$baseDomain, '', $host);
-                    dd($host) ;
+                    if (in_array($host, [$baseDomain, 'www.' . $baseDomain]))
+                        {
+                            return $next($request);
+                        }
                     $tenant = Microsite::where('domain', $host)
                                        ->where('status', 1)
                                        ->first();
 
                     if (!$tenant)
-                    {
-                        return redirect()->route('home');
-                    }
+                        {
+                            return redirect()->route('home');
+                        }
 
                     // Make tenant globally available
                     app()->instance('tenant', $tenant);
