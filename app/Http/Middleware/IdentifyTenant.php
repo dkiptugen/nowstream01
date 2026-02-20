bdb5fad794d37ac4711217c9477db852040095a5
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-02-20 14:35:35 +0300

    erere

--- app/Http/Middleware/IdentifyTenant.php
+++ app/Http/Middleware/IdentifyTenant.php
@@ -26,7 +26,7 @@
                         }
 
                     // Make tenant globally available
-                    app()->instance('tenant', $tenant);
+                    app()->instance('tenant', collect($tenant));
 
                     return $next($request);
                 }
