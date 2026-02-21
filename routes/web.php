e8418506603693a912ab821bf19273049b4a8964
oprestone <oprestone0@gmail.com>
2026-02-21 10:11:28 +0300

    changes

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,7 @@
     use App\Http\Controllers\Frontend\HomeController;
     use App\Http\Controllers\Frontend\EventController;
     use App\Http\Controllers\Auth\User\AuthsController;
-use App\Http\Controllers\Backend\MicrositeController;
-use App\Http\Controllers\Frontend\MicrositeController as FrontendMicrositeController;
-use App\Http\Controllers\Frontend\PodcastController as FrontendPodcastController;
+    use App\Http\Controllers\Frontend\PodcastController as FrontendPodcastController;
     use App\Http\Controllers\Frontend\SearchController;
     use App\Http\Controllers\Frontend\TVController;
     use App\Http\Controllers\Frontend\RadioController;
@@ -49,7 +47,7 @@ use App\Http\Controllers\Frontend\PodcastController as FrontendPodcastController
                  ->controller(TenantController::class)
                  ->group(function ()
                     {
-                        Route::get('/', [FrontendMicrositeController::class, 'index'])->name('home');
+                        Route::get('/', 'index')->name('home');
                         Route::get('/events', 'events')->name('events');
                         Route::get('/event/{event}', 'single_event')->name('single_event');
                         Route::get('/streams', 'streams')->name('streams');
