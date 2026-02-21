9f7a71d36c58ae97345529546f7da978cc168ad5
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-02-21 21:41:12 +0300

    check

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,34 +1,25 @@
 <?php
 
-namespace Database\Seeders;
+    namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Channel;
-use App\Models\Event;
-use App\Models\Content;
-use App\Models\ContentPartner;
-use App\Models\Video;
-use Illuminate\Database\Seeder;
+    // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+    use App\Models\Channel;
+    use App\Models\Event;
+    use App\Models\Content;
+    use App\Models\ContentPartner;
+    use App\Models\Video;
+    use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        $this->call([UserSeeder::class,ChannelSeeder::class,SystemUserChannelSeeder::class]);
-        // \App\Models\User::factory(10)->create();
+    class DatabaseSeeder extends Seeder
+        {
+        /**
+         * Seed the application's database.
+         */
+            public function run()
+            : void
+                {
+                    $this->call([UserSeeder::class]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        /*ContentPartner::factory(300)->create();
-        Channel::factory(1500)->create();
-        Event::factory(1500)->create();
-		Content::where('type', 'video')->factory(3000)->create();
-		Content::factory (3000)->create();*/
 
-    }
-}
+                }
+        }
