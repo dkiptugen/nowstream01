ca8c53ba3b2e45a2e8135247d3d5f19f5441b711
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-02-22 16:53:53 +0300

    test

--- resources/views/Backend/modules/episode/show.blade.php
+++ resources/views/Backend/modules/episode/show.blade.php
@@ -33,7 +33,7 @@
 
                         <!-- VIDEO -->
                     <div class="ratio ratio-16x9">
-                        <video id="player" playsinline data-poster="{{ $episode->thumbnail_url }}" banner="{{ $episode->thumbnail_url }}" controls">
+                        <video id="player" playsinline data-poster="{{ $episode->thumbnail_url }}" data-banner="{{ $episode->thumbnail_url }}" controls">
                             <source src="{{ $episode->stream_url }}" type="{{ $mime }}">
                         </video>
                     </div>
