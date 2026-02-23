b3f081c727329f5039a78012f224dff02f6e6e0a
oprestone <oprestone0@gmail.com>
2026-02-24 01:49:36 +0300

    xcv

--- resources/views/Frontend/modules/genres/show.blade.php
+++ resources/views/Frontend/modules/genres/show.blade.php
@@ -41,5 +41,46 @@
             @endif
         </div>
     </section>
+       <section class="top-rated-movie tr-movie-bg" data-background="{{ asset('assets/img')}}/bg/tr_movies_bg.jpg">
+        <div class="container">
+            <div class="episode-top-wrap">
+                <div class="section-title"> <span class="sub-title">Trending Radios</span>
+                    <h2 class="title">Trending Radios</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="pcar-wrapper">
+
+            <!-- Outside container overlays -->
+            <div class="pcar-overlay pcar-overlay-left"></div>
+            <div class="pcar-overlay pcar-overlay-right"></div>
+
+            <div class="pcar" data-autoplay="true" data-interval="3500" data-desktop="11" data-tablet="3"
+                data-mobile="1">
+
+                <div class="pcar-track">
+                   
+                </div>
+            </div>
+        </div>
+        <div class="container mt-md-5">
+            <div class="episode-top-wrap">
+                <div class="section-title"> <span class="sub-title">Latest radios</span>
+                    <h2 class="title">Latest radios</h2>
+                </div>
+            </div>
+
+            <div class="row tr-movie-active h-100" id="radio-container" style="position: relative; height:auto !important;">
+                @include('Frontend.includes.components.partials.radio-items', ['radios' => $contents])
+            </div>
+
+            <div class="text-center my-4" id="loading" style="display:none;">
+                <span class="text-light">Loading more radios...</span>
+            </div>
+
+        </div>
+        </div>
+    </section>
 </main>
 @endsection
--- resources/views/Frontend/modules/radios/index.blade.php
+++ resources/views/Frontend/modules/radios/index.blade.php
@@ -1,23 +1,23 @@
 @extends('Frontend.includes.layout')
 @section('content') <!-- main-area -->
-    <main> <!-- breadcrumb-area -->
-        <section class="breadcrumb-area breadcrumb-bg" data-background="{{ asset('assets/img/bg/breadcrumb_bg.jpg') }}">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="breadcrumb-content">
-                            <h2 class="title">Live <span>radios</span></h2>
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="{{'/'}}">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">radios</li>
-                                </ol>
-                            </nav>
-                        </div>
+<main> <!-- breadcrumb-area -->
+    <section class="breadcrumb-area breadcrumb-bg" data-background="{{ asset('assets/img/bg/breadcrumb_bg.jpg') }}">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb-content">
+                        <h2 class="title">Live <span>radios</span></h2>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{'/'}}">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">radios</li>
+                            </ol>
+                        </nav>
                     </div>
                 </div>
+            </div>
 
-                 <div class="ucm-nav-wrap">
+            <div class="ucm-nav-wrap">
                 <ul class="nav nav-tabs" id="genreTabs" role="tablist">
                     @foreach($genres->filter()->unique() as $genre)
                     @php
@@ -34,55 +34,55 @@
                     @endforeach
                 </ul>
             </div>
+        </div>
+    </section> <!-- breadcrumb-area-end -->
+    <section class="top-rated-movie tr-movie-bg" data-background="{{ asset('assets/img')}}/bg/tr_movies_bg.jpg">
+        <div class="container">
+            <div class="episode-top-wrap">
+                <div class="section-title"> <span class="sub-title">Trending Radios</span>
+                    <h2 class="title">Trending Radios</h2>
+                </div>
             </div>
-        </section> <!-- breadcrumb-area-end -->
-        <section class="top-rated-movie tr-movie-bg" data-background="{{ asset('assets/img')}}/bg/tr_movies_bg.jpg">
-            <div class="container">
-                <div class="episode-top-wrap">
-                    <div class="section-title"> <span class="sub-title">Trending Radios</span>
-                        <h2 class="title">Trending Radios</h2>
+        </div>
+
+        <div class="pcar-wrapper">
+
+            <!-- Outside container overlays -->
+            <div class="pcar-overlay pcar-overlay-left"></div>
+            <div class="pcar-overlay pcar-overlay-right"></div>
+
+            <div class="pcar" data-autoplay="true" data-interval="3500" data-desktop="11" data-tablet="3"
+                data-mobile="1">
+
+                <div class="pcar-track">
+                    @foreach($topradios as $item)
+                    <div class="pcar-item">
+                        @include('Frontend.includes.components.cards.slider-card')
                     </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        <div class="container mt-md-5">
+            <div class="episode-top-wrap">
+                <div class="section-title"> <span class="sub-title">Latest radios</span>
+                    <h2 class="title">Latest radios</h2>
                 </div>
             </div>
 
-            <div class="pcar-wrapper">
-
-                <!-- Outside container overlays -->
-                <div class="pcar-overlay pcar-overlay-left"></div>
-                <div class="pcar-overlay pcar-overlay-right"></div>
-
-                <div class="pcar" data-autoplay="true" data-interval="3500" data-desktop="11" data-tablet="3"
-                    data-mobile="1">
-
-                    <div class="pcar-track">
-                        @foreach($topradios as $item)
-                            <div class="pcar-item">
-                                @include('Frontend.includes.components.cards.slider-card')
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
+            <div class="row tr-movie-active h-100" id="radio-container" style="position: relative; height:auto !important;">
+                @include('Frontend.includes.components.partials.radio-items', ['radios' => $radios])
             </div>
-            <div class="container mt-md-5">
-                <div class="episode-top-wrap">
-                    <div class="section-title"> <span class="sub-title">Latest radios</span>
-                        <h2 class="title">Latest radios</h2>
-                    </div>
-                </div>
 
-                <div class="row tr-movie-active h-100" id="radio-container" style="position: relative; height:auto !important;">
-                    @include('Frontend.includes.components.partials.radio-items', ['radios' => $radios])
-                </div>
-
-                <div class="text-center my-4" id="loading" style="display:none;">
-                    <span class="text-light">Loading more radios...</span>
-                </div>
-
+            <div class="text-center my-4" id="loading" style="display:none;">
+                <span class="text-light">Loading more radios...</span>
             </div>
-            </div>
-            </section> 
 
-    </main>
+        </div>
+        </div>
+    </section>
+
+</main>
 @endsection
 @section('header')
 @endsection
