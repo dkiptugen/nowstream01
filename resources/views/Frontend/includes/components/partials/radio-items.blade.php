1e55dd4b37ecb3c10012c91ca26ca15c4ce7d18f
oprestone <oprestone0@gmail.com>
2026-02-24 01:59:25 +0300

    xcv

--- resources/views/Frontend/includes/components/partials/radio-items.blade.php
+++ resources/views/Frontend/includes/components/partials/radio-items.blade.php
@@ -1,5 +1,5 @@
 @foreach($radios as $item)
-<div class="col-xl-2 col-lg-3 col-sm-6 grid-item d-block">
+<div class="col-xl-2 col-lg-3 col-sm-6 grid-item" style="position: relative !important;">
     @include('Frontend.includes.components.cards.slider-card') 
 </div>
 @endforeach
