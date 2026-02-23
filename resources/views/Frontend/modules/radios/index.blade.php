c4e9dc9a3910501f78a156e4deb72b331eadfa87
oprestone <oprestone0@gmail.com>
2026-02-24 02:03:33 +0300

    xcv

--- resources/views/Frontend/modules/radios/index.blade.php
+++ resources/views/Frontend/modules/radios/index.blade.php
@@ -85,6 +85,11 @@
 </main>
 @endsection
 @section('header')
+<style>
+    .col-xl-2.col-lg-3.col-sm-6.grid-item{
+        position: relative !important;
+    }
+</style>
 @endsection
 @section('footer')
 
