e9def4a5627116c8537182bca88ac010c084803f
oprestone <oprestone0@gmail.com>
2026-02-24 03:28:24 +0300

    xcv

--- resources/views/Frontend/includes/components/partials/audio-player.blade.php
+++ resources/views/Frontend/includes/components/partials/audio-player.blade.php
@@ -231,21 +231,6 @@
          window.playGlobalAudio = function(list, index = 0) {
              if (!Array.isArray(list) || !list.length) return;
              playlist = list;
-
-             // Increment views
-             if (track?.uuid) {
-                 fetch(`/content/${track.uuid}/increment-views`, {
-                         method: 'POST',
-                         headers: {
-                             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
-                             'Accept': 'application/json',
-                             'Content-Type': 'application/json'
-                         },
-                     })
-                     .then(res => res.json())
-                     .then(data => console.log('Episode views incremented', data.views))
-                     .catch(err => console.error('Error incrementing views', err));
-             }
              loadTrack(index);
          };
 
