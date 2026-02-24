abf5d6eb10a2a35d09df436b067940c779d75d9d
oprestone <oprestone0@gmail.com>
2026-02-24 06:36:42 +0300

    df

--- resources/views/Frontend/includes/header.blade.php
+++ resources/views/Frontend/includes/header.blade.php
@@ -169,7 +169,15 @@
         }
     </style>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FT13EMDEPD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-FT13EMDEPD');
+</script>
 
 </head>
 
