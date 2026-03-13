0fa1a6258ef7a0589ceeff875be917ab6664621f
oprestone <oprestone0@gmail.com>
2026-03-13 11:35:39 +0300

    dv

--- resources/views/Frontend/includes/components/partials/audio-player.blade.php
+++ resources/views/Frontend/includes/components/partials/audio-player.blade.php
@@ -1,5 +1,10 @@
  <div id="global-audio-player" class="spotify-player d-none">
 
+         <div class="sp-progress-wrap d-md-none w-100 mt-2">
+             <span id="sp-current">0:00</span>
+             <input type="range" id="player-progress" value="0">
+             <span id="sp-duration">0:00</span>
+         </div>
      <div class="sp-left">
          <img id="player-thumbnail" class="sp-artwork" src="" alt="">
          <div class="sp-meta">
@@ -37,11 +42,6 @@
          <input type="range" id="player-volume" min="0" max="1" step="0.01">
      </div>
 
-         <div class="sp-progress-wrap d-md-none w-100 mt-2">
-             <span id="sp-current">0:00</span>
-             <input type="range" id="player-progress" value="0">
-             <span id="sp-duration">0:00</span>
-         </div>
      <audio id="global-audio"></audio>
  </div>
 <script>
