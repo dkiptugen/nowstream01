b1937cf7d82e50a19a69d6e0874e78532c531dba
oprestone <oprestone0@gmail.com>
2026-03-22 13:54:12 +0300

    dv

--- resources/views/Frontend/modules/events/event.blade.php
+++ resources/views/Frontend/modules/events/event.blade.php
@@ -77,7 +77,7 @@ $thumbnail = $event->event_image ? Storage::disk(config('filesystems.default'))-
 										</td>
 										<td class="align-content-center">
 											<!-- {{ $currency }} -->
-											  {{ $rate->currency : $country == 'KE' ? 'KES ' : config('custom.BILLING.RESERVED_CURRENCY') . ' ' }}{{ $rate->price }}
+											  {{ $rate->currency }}{{ $rate->price }}
 										</td>
 										<td class="align-content-center text-end">
 											<a class="btn btn-sm btn-success p-2 pl-3"
