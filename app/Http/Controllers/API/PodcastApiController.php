7eb99750f53e2436e67fc61b2a746027650b0ccd
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-04-06 11:41:58 +0300

    check

--- app/Http/Controllers/API/PodcastApiController.php
+++ app/Http/Controllers/API/PodcastApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 
 class PodcastApiController extends Controller
@@ -11,7 +12,7 @@ class PodcastApiController extends Controller
             {
                 return response()->json(['message' => 'Hello from API!']);
             }
-        public function show()
+        public function show(Request $request, $slug)
             {
                 return response()->json(['message' => 'Hello from API!']);
             }
