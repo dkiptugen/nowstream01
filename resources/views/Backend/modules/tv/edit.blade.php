a7a873859f78b0dd16f028350f8976ae19d1fa8f
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-04-10 14:37:47 +0300

    dffdf

--- resources/views/Backend/modules/tv/edit.blade.php
+++ resources/views/Backend/modules/tv/edit.blade.php
@@ -14,27 +14,34 @@
                         @csrf
                         <div class="mb-3">
                             <label for="title" class="form-label">Title</label>
-                            <input type="text" name="title" id="title" class="form-control" required value="{{ $tv->title??old('title') }}">
+                            <input type="text" name="title" id="title" class="form-control" required
+                                   value="{{ $tv->title??old('title') }}">
                         </div>
                         <div class="mb-3">
                             <label for="description" class="form-label">Description</label>
-                            <textarea rows="4" name="description" id="description" data-ckeditor class="form-control">{{ $tv->description??old('description') }}</textarea>
+                            <textarea rows="4" name="description" id="description" data-ckeditor
+                                      class="form-control">{{ $tv->description??old('description') }}</textarea>
                         </div>
-                        <div class="mb-3">
-                            <label for="thumbnail" class="form-label">Thumbnail</label> <input
-                                type="file" name="thumbnail" id=""
-                                class="form-control @error('thumbnail') is-invalid @enderror"> <small
-                                class="text-muted">Should be 150x150PX</small>
-                            @error('thumbnail')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
+                        <div class="mb-3 d-flex align-items-center">
+                            <img src="{{ $tv->thumbnail_url }}" alt="{{ $tv->title }}" class="img-fluid" width="150"
+                                 height="150">
+                            <div class="form-input">
+                                <label for="thumbnail" class="form-label">Thumbnail</label> <input
+                                    type="file" name="thumbnail" id=""
+                                    class="form-control @error('thumbnail') is-invalid @enderror"> <small
+                                    class="text-muted">Should be 150x150PX</small>
+                                @error('thumbnail')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
                         <div class="row mb-3 g-3">
                             <div class="col">
                                 <label for="country" class="form-label">Country</label>
                                 <select name="country" id="country" class="js-choice form-control">
                                     @foreach($regions as $region)
-                                        <option value="{{ $region->id }}" @selected($tv->region_id == $region->id)>{{ $region->name }}</option>
+                                        <option
+                                            value="{{ $region->id }}" @selected($tv->region_id == $region->id)>{{ $region->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -42,7 +49,8 @@
                                 <label for="country" class="form-label">Language</label>
                                 <select name="language" id="language" class="js-choice form-control">
                                     @foreach($languages as $language)
-                                        <option value="{{ $language->id }}" @selected($tv->language_id == $language->id)>{{ $language->name }}</option>
+                                        <option
+                                            value="{{ $language->id }}" @selected($tv->language_id == $language->id)>{{ $language->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -50,7 +58,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="stream_url" class="form-label">Stream Url</label>
-                            <input type="text" name="stream_url" id="stream_url" class="form-control" value="{{ $tv->stream_url??old('stream_url') }}" />
+                            <input type="text" name="stream_url" id="stream_url" class="form-control"
+                                   value="{{ $tv->stream_url??old('stream_url') }}"/>
                         </div>
                         <div class="d-flex justify-content-end mt-2">
                             <button type="submit" class="btn btn-primary">Edit TV</button>
