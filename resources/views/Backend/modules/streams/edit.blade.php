9586794b30a10bd5d2cc282804e4624566293e46
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-04-10 14:14:33 +0300

    dffdf

--- resources/views/Backend/modules/streams/edit.blade.php
+++ resources/views/Backend/modules/streams/edit.blade.php
@@ -1,5 +1,15 @@
 @extends('Backend.includes.layout')
-@section('content') <div class="row"> <div class="col "> <div class="card shadow-lg border"> <div class="card-header"> <h3 class="my-0 card-title h5"> Edit Stream </h3> </div> <div class="card-body"> <form action="" class="form " method="post"> </form> </div> </div> </div> </div>
+@section('content')
+    <div class="row">
+        <div class="col ">
+            <div class="card shadow-lg border">
+                <div class="card-header"><h3 class="my-0 card-title h5"> Edit Stream </h3></div>
+                <div class="card-body">
+                    <form action="" class="form " method="post"></form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('header')
 @endsection
--- resources/views/Backend/modules/tv/edit.blade.php
+++ resources/views/Backend/modules/tv/edit.blade.php
@@ -1,5 +1,65 @@
 @extends('Backend.includes.layout')
-@section('content') <div class="row"> <div class="col "> <div class="card shadow-lg border"> <div class="card-header"> <h3 class="my-0 card-title h5"> Edit Stream </h3> </div> <div class="card-body"> <form action="" class="form " method="post"> </form> </div> </div> </div> </div>
+@section('content')
+    <div class="row">
+        <div class="col ">
+            <div class="d-flex align-items-center justify-content-between mb-3">
+                <div>
+                    <h1 class="h3 mb-1">Tvs</h1>
+                    <div class="text-muted">Edit Television</div>
+                </div>
+            </div>
+            <div class="card shadow-lg border">
+                <div class="card-body">
+                    <form action="" class="form " method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Title</label>
+                            <input type="text" name="title" id="title" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea rows="4" name="description" id="description" class="form-control"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="thumbnail" class="form-label">Thumbnail</label> <input
+                                type="file" name="thumbnail" id=""
+                                class="form-control @error('thumbnail') is-invalid @enderror"> <small
+                                class="text-muted">Should be 150x150PX</small>
+                            @error('thumbnail')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="row mb-3 g-3">
+                            <div class="col">
+                                <label for="country" class="form-label">Country</label>
+                                <select name="country" id="country" class="js-select">
+                                    @foreach($regions as $region)
+                                        <option value="{{ $region->id }}">{{ $region->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col">
+                                <label for="country" class="form-label">Language</label>
+                                <select name="language" id="language" class="js-select">
+                                    @foreach($languages as $language)
+                                        <option value="{{ $language->id }}">{{ $language->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="stream_url" class="form-label">Stream Url</label>
+                            <input type="text" name="stream_url" id="stream_url" class="form-control" />
+                        </div>
+                        <div class="d-flex justify-content-end mt-2">
+                            <button type="submit" class="btn btn-sm btn-primary">Add Video</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('header')
 @endsection
