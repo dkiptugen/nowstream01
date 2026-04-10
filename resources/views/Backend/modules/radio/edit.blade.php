e1ccf64e34a76b976afb5618d55596fe8b76ba1b
Dennis Kiptoo Kiptugen <dennis.kiptugen@thekenyatimes.com>
2026-04-10 14:24:18 +0300

    dffdf

--- resources/views/Backend/modules/radio/edit.blade.php
+++ resources/views/Backend/modules/radio/edit.blade.php
@@ -34,7 +34,7 @@
                         <div class="row mb-3 g-3">
                             <div class="col">
                                 <label for="country" class="form-label">Country</label>
-                                <select name="country" id="country" class="js-select">
+                                <select name="country" id="country" class="js-choice form-control">
                                     @foreach($regions as $region)
                                         <option value="{{ $region->id }}">{{ $region->name }}</option>
                                     @endforeach
@@ -42,7 +42,7 @@
                             </div>
                             <div class="col">
                                 <label for="country" class="form-label">Language</label>
-                                <select name="language" id="language" class="js-select">
+                                <select name="language" id="language" class="js-choice form-control">
                                     @foreach($languages as $language)
                                         <option value="{{ $language->id }}">{{ $language->name }}</option>
                                     @endforeach
